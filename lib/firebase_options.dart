8e0b488a091b3f20b76ac31896225874fa249501
parmenin <quizblog.rw@gmail.com>
2024-09-16 19:22:44 +0200

    gradle changes, env, app ID, fixes, swicth pdf viewer

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 /// Default Firebase configuration options for the current platform.
 class DefaultFirebaseOptions {
@@ -14,23 +15,6 @@ class DefaultFirebaseOptions {
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
         return android;
-      case TargetPlatform.iOS:
-        return ios;
-      case TargetPlatform.macOS:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for macos - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
-      case TargetPlatform.windows:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for windows - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
-      case TargetPlatform.linux:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for linux - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
       default:
         throw UnsupportedError(
           'DefaultFirebaseOptions are not supported for this platform.',
@@ -38,22 +22,11 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyBpmCP0X8--RA3xmgwvdRH8p4ckH70_56I',
-    appId: '1:1080577461306:android:b335d3ced0dbadd8b8f903',
-    messagingSenderId: '1080577461306',
-    projectId: 'tegura-rw',
-    storageBucket: 'tegura-rw.appspot.com',
-  );
-
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyBtvrbE_PhQDmY_mRVAZ2DnXfjbU6NtWds',
-    appId: '1:1080577461306:ios:fcc099783162d587b8f903',
-    messagingSenderId: '1080577461306',
-    projectId: 'tegura-rw',
-    storageBucket: 'tegura-rw.appspot.com',
-    iosClientId:
-        '1080577461306-ng1nidkiuq62jub56othtrdigj13tfnu.apps.googleusercontent.com',
-    iosBundleId: 'rw.quizblog.itsindire',
+  static FirebaseOptions android = FirebaseOptions(
+    apiKey: dotenv.env['apiKey'] ?? '',
+    appId: dotenv.env['appId'] ?? '',
+    messagingSenderId: dotenv.env['messagingSenderId'] ?? '',
+    projectId: dotenv.env['projectId'] ?? '',
+    storageBucket: dotenv.env['storageBucket'] ?? '',
   );
 }
